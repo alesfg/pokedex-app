28df726270c2c8c390e4fabc8bb310ff96b2fc63
Alex Flores <59764503+alesfg@users.noreply.github.com>
2021-12-10 21:02:17 +0100

    Add files via upload

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { FlatList, SafeAreaView, StyleSheet, Text } from 'react-native';
+import { SafeAreaView, StyleSheet, Text } from 'react-native';
 
 import axios from 'axios'
 
@@ -15,8 +15,9 @@ export default function App() {
   const [nextPageUrl, setNextPageUrl] = useState();
   const [prevPageUrl, setPrevPageUrl] = useState();
   const [loading, setLoading] = useState(true);
-  const [pokemonIndex, setPokemonIndex] = useState([]);
 
+  const [pokemonIndex, setPokemonIndex] = useState([]);
+  const [pokemonUrl, setPokemonUrl] =useState([])
 
   useEffect(() => {
     setLoading(true)
@@ -25,35 +26,36 @@ export default function App() {
       cancelToken: new axios.CancelToken(c => cancel = c)
     }).then(res => {
       setLoading(false)
+      console.log(res.data)
       setNextPageUrl(res.data.next)
       setPrevPageUrl(res.data.previous)
       setPokemon(res.data.results.map(p=>p.name))
-      setPokemonIndex(res.data.results.map(p=>p.url.split("/")[p.url.split('/').length - 2]));
+
+      setPokemonIndex(res.data.results.map(p=>p.url.split("/")[p.url.split('/').length - 2]))
+      // setPokemonUrl(`https://pokeapi.co/api/v2/pokemon/${res.data.results.map(p=>p.name)}`)
+      // setPokemonUrl(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+      console.log("pokee"+pokemon)
     })
     return () => cancel()
   }, [currentPageUrl])
-
   function gotoNextPage() {
     setCurrentPageUrl(nextPageUrl)
   }
   function gotoPrevPage() {
     setCurrentPageUrl(prevPageUrl)
   }
-   
+   console.log("asd"+pokemonUrl)
 
   if(loading) return <Text>"Cargando Pokédex🚗🛸"</Text>
-
   return (
     <SafeAreaView style={styles.container}>
-      {/* <FlatList
-        data={[<PokemonList pokemon={pokemon}/>]}
-      /> */}
-        <PokemonList pokemon={pokemon} pokemonIndex={pokemonIndex}/>
+        <PokemonList pokemon={pokemon} pokemonIndex={pokemonIndex} />
 
         <Pagination 
           gotoNextPage={nextPageUrl ? gotoNextPage : null}
           gotoPrevPage={prevPageUrl ? gotoPrevPage : null}
           />
+          {/* <Image source={pokemonImage} /> */}
     </SafeAreaView>
   );
 }
--- components/PokemonList.js
+++ components/PokemonList.js
@@ -1,22 +1,54 @@
-import React, { useState } from 'react'
+import React from 'react'
 
-import { StyleSheet, View, Text, Image } from 'react-native';
+import { StyleSheet, View, Text, Image, ScrollView } from 'react-native';
 
 
 export default function PokemonList({ pokemon,pokemonIndex }) {
     
-    const [url, setUrl] = useState(`https://pokeapi.co/api/v2/pokemon`)
-    // let pokemonInde=pokemonIndex.map();
-    // const imageUrl = `https://github.com/PokeAPI/sprites/blob/master/sprites/pokemon/${pokemonIndex}.png`
-    console.log("index2:"+pokemonIndex)
+    const url = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/`
+    let urlPoke = 'https://pokeapi.co/api/v2/pokemon/';
+
+    // pokemon solo tiene 20 nombres cada render, pero p(pokemonIndex) sigue sumando, entonces cuando quiero acceder al nombre
+    // necesito el index porque las imagenes son por ej 124.png pero ahora tb necesito otro iterador Bien para acceder al resto de datos de Pokeapi
     return (
-      <View>
-        {pokemon.map(p => (
-          <View key={p}>
-            <Text>{p}</Text>                          
+      <ScrollView>
+        {pokemonIndex.map(p => (
+          <View key={p} style={styles.card}>
+            {/* {console.log("este p:"+p)} */}
+            {/* {console.log("este poke:"+pokemon[p-1])} */}
+            <Text style={styles.digits}>
+                # {p.length == 1 ? '00' : ''}
+                  {p.length == 2 ? '0' : ''}
+                {p}
+            </Text>
+            <Text style={styles.text}> {pokemon[p-1]} </Text>
+            
+            <Image 
+              source= {{ uri: `${url}${p}.png` }}
+              style= {styles.image}
+              />
+              <Image 
+              source= {{ uri: `${url}back/${p}.png` }}
+              style= {styles.image}
+              />
+              <Image 
+              source= {{ uri: `${url}other/official-artwork/${p}.png` }}
+              style= {styles.image}
+              resizeMode='contain'
+              /> 
+              <Image 
+              source= {{ uri: `${url}other/dream-world/${p}.svg` }}
+              style= {styles.image}
+              resizeMode='contain'
+              />
+              <Image 
+              source= {{ uri: `${url}other/home/${p}.png` }}
+              style= {styles.image}
+              />
+              
           </View>      
         ))}  
-        </View>
+        </ScrollView>
         
     )
 }
@@ -41,4 +73,14 @@ const styles = StyleSheet.create({
       width: 100,
       height: 100,
     },
+    digits: {
+      fontSize:10,
+      color: 'grey',
+      backgroundColor: 'white',
+      height:'min-content'
+    },
+    text: {
+      textTransform: 'capitalize',
+      fontSize: 16
+    }
   });
