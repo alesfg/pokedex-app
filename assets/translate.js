1ef7ddc2219bd1dd5a0e0fd12199c05d45328c07
Alex Flores <59764503+alesfg@users.noreply.github.com>
2022-02-10 19:59:37 +0100

    translate.js refactor

--- assets/translate.js
+++ assets/translate.js
@@ -1,61 +1,22 @@
-const translateType = (type) => {
-    //  const t= type == "fire" ? 'fuego' : type,
-          switch (type) {
-            case "fire":
-              return "fuego"
-              break;
-            case "flying":
-              return "volador"
-              break
-            case "grass":
-              return "planta"
-              break
-            case "poison":
-              return "veneno"
-              break
-            case "water":
-              return "agua"
-              break
-            case "bug":
-              return "bicho"
-              break
-            case "fighting":
-              return "luchador"
-              break
-            case "rock":
-              return "roca"
-              break
-            case "psychic":
-              return "psíquico"
-              break
-            case "electric":
-              return "eléctrico"
-              break
-            case "fairy":
-              return "hada"
-              break
-            case "ice":
-              return "hielo"
-              break
-            case "dragon":
-              return "dragón"
-              break
-            case "dark":
-              return "oscuridad"
-              break
-            case "ground":
-              return "tierra"
-              break
-            case "ghost":
-              return "fantasma"
-              break
-            case "steel":
-              return "acero"
-              break
-            default:
-              return type
-              break;
-          }
+export const translateType = (type) => {
+    const tipos = {
+      fire : "fuego",
+      flying : "volador",
+      grass : "planta",
+      poison : "veneno",
+      water : "agua",
+      bug : "bicho",
+      fighting : "luchador",
+      rock : "roca",
+      ground: "tierra",
+      psychic : "psíquico",
+      electric : "eléctrico",
+      fairy : "hada",
+      ice : "hielo",
+      dragon : "dragón",
+      dark : "oscuridad",
+      ghost : "fantasma",
+      steel : "acero"
     }
-
-    export { translateType }
+    return  tipos[type];
+}
